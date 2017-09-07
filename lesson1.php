93f52acb9fea65123be2b1a5bef2cfb469ac9334
lorafbm <gsl_billdind@mail.ru>
2017-09-07 23:49:43 +0300

    Initial commit

---
+++ lesson1.php
@@ -0,0 +1,21 @@
+<?php
+//echo 'Hello World';
+?>
+<?php $name = 12;  $name2 = 10; $s = $name1+$name2; echo $s.'<br/>'; ?>
+<?php $str = 'Hello'; ?>
+<?php
+$a = 15;
+$b = '5';
+//echo $a/$b;
+//var_dump((int)$b);
+//echo $a;
+$str = 'Hello';
+$str2 = 'World';
+//echo $str.' '.$str2;
+$i = 1;
+echo ++$i.'<br/>';
+
+echo $i++.'<br/>';
+
+?>
+
