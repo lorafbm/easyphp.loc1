a60b02314e4708786d6d09b2c94a618bd18e4d21
lorafbm <gsl_billdind@mail.ru>
2017-09-19 19:13:47 +0300

    initial commit

--- lesson4.php
+++ lesson4.php
@@ -1,4 +1,23 @@
 <?php
 error_reporting(-1);
 echo 'занятие №4 Функции<br>';
+$name = 'abc';
+function hello($name){
+    return 'Hello, '.$name;
+}
+$name = 'Lora';
+ //echo hello($name);
+
+function num($a,$b,$c = 0,$d = 0){
+    if (Sa>$b){
+        return 'a > b';
+    }elseif($a == $b){
+        return 'A = b';
+    }else{
+        return 'a < b';
+    }
+}
+
+
+
 
